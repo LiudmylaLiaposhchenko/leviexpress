01d806831cc9f6f48f73e8986aedcb56e5fb64f8
LL <liaposhchenkol@dmail.com>
2024-01-01 14:14:19 +0100

    Fetch dates from the API

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -6,6 +6,7 @@ export const JourneyPicker = ({ onJourneyChange }) => {
   const [toCity, setToCity] = useState('');
   const [date, setDate] = useState('');
   const [cities, setCities] = useState([]);
+  const [dates, setDates] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -14,6 +15,16 @@ export const JourneyPicker = ({ onJourneyChange }) => {
       );
       const data = await response.json();
       setCities(data.results);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        'https://apps.kodim.cz/daweb/leviexpress/api/dates',
+      );
+      const data = await response.json();
+      setDates(data.results);
     })();
   }, []);
 
@@ -45,12 +56,7 @@ export const JourneyPicker = ({ onJourneyChange }) => {
           <label>
             <div className="journey-picker__label">Datum:</div>
             <select value={date} onChange={(e) => setDate(e.target.value)}>
-              <option value="">Vyberte</option>
-              <option value="datum01">Datum 01</option>
-              <option value="datum02">Datum 02</option>
-              <option value="datum03">Datum 03</option>
-              <option value="datum04">Datum 04</option>
-              <option value="datum05">Datum 05</option>
+              <DatesOptions dates={dates} />
             </select>
           </label>
           <div className="journey-picker__controls">
@@ -65,7 +71,7 @@ export const JourneyPicker = ({ onJourneyChange }) => {
   );
 };
 
-export const CityOptions = ({ cities }) => {
+const CityOptions = ({ cities }) => {
   return (
     <>
       <option value="">Vyberte</option>
@@ -73,6 +79,21 @@ export const CityOptions = ({ cities }) => {
         return (
           <option key={c.code} value={c.code}>
             {c.name}
+          </option>
+        );
+      })}
+    </>
+  );
+};
+
+const DatesOptions = ({ dates }) => {
+  return (
+    <>
+      <option value="">Vyberte</option>
+      {dates.map((d) => {
+        return (
+          <option key={d.dateBasic} value={d.dateBasic}>
+            {d.dateCs}
           </option>
         );
       })}
